b4e03045c853650c37277eb9966acdbfeccba22c
ANAMONTEIROMISQUITA <anaclaudia.misquita@gmail.com>
2025-10-20 12:19:35 -0300

    update rpa

--- rpa.py
+++ rpa.py
@@ -44,3 +44,37 @@ def coletar_dados_rick():
 
     else:
         print(f"❌ Erro ao acessar API: {response.status_code}")
+
+def filtrar_dados_processados():
+    conexao = sqlite3.connect("projeto_rpa.db")
+    cursor = conexao.cursor()
+
+    regex_status = re.compile(r'^Alive$', re.IGNORECASE)
+    regex_genero = re.compile(r'^Male$', re.IGNORECASE)
+
+    cursor.execute("SELECT * FROM ricks")
+    dados = cursor.fetchall()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS dados_processados (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            status TEXT,
+            species TEXT,
+            gender TEXT
+        )
+    """)
+
+    for dado in dados:
+        id_, nome, status, especie, genero = dado
+
+        if regex_status.match(status) and regex_genero.match(genero):
+            cursor.execute("""
+                INSERT OR IGNORE INTO dados_processados (id, name, status, species, gender)
+                VALUES (?, ?, ?, ?, ?)
+            """, (id_, nome, status, especie, genero))
+            print(f"✅ Salvo em dados_processados: {nome} - {status} - {genero}")
+
+    conexao.commit()
+    conexao.close()
+    print("✅ Dados filtrados e salvos com sucesso!")
